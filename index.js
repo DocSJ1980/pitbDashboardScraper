7896945433473af7ac216b1d2dafd4c68112dc12
DocSJ1980 <docsaj@outlook.com>
2022-09-07 07:30:18 -0700

    First Page scraped successfully

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const puppeteer = require('puppeteer-extra')
 const fs = require('fs/promises')
 const writeFileSync = require("fs").writeFileSync;
-const { tableParser } = require('puppeteer-table-parser');
-const axios = require('axios')
 const cheerio = require('cheerio')
 
 // add recaptcha plugin and provide it your 2captcha token (= their apiKey)
@@ -21,12 +19,15 @@ puppeteer.use(
     })
 )
 // puppeteer usage as normal
+// Launch Browser
 puppeteer.launch({
     headless: false,
     defaultViewport: null,
     executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 })
+    // Scraping Logic
     .then(async browser => {
+        //Login
         const page = await browser.newPage()
         await page.goto('https://dashboard.tracking.punjab.gov.pk')
         await page.type("#user_username", "web.dept.dcorawalpindi.10029")
@@ -38,6 +39,7 @@ puppeteer.launch({
             page.waitForNavigation(),
             page.click(`#new_user button`)
         ])
+        // Navigating to Activities page
         await page.goto('https://dashboard.tracking.punjab.gov.pk/activities/simples/line_list')
         await page.type("#department", "DHA Rawalpindi")
         await page.keyboard.press("Tab");
@@ -78,6 +80,7 @@ puppeteer.launch({
             page.waitForNavigation(),
             page.click('body > div.app-admin-wrap.layout-horizontal-bar.clearfix > div > div.row.p-custom > div:nth-child(5) > button'),
         ])
+        // Loading HTML data in cheerio and selecting table
         const pageData = await page.evaluate(() => {
             return { html: document.documentElement.innerHTML }
         })
@@ -86,24 +89,9 @@ puppeteer.launch({
         const table = $('#p_table')
         const rowSelector = "#p_table > tbody > tr"
 
-        const keys = [
-            'sr',
-            'id',
-            'district',
-            'town',
-            'uc',
-            'tag',
-            'larva',
-            'dengueLarva',
-            'lat',
-            'long',
-            'pics',
-            'timeDiff',
-            'user',
-            'activityTime',
-            'bogus'
-        ]
+        const keys = ['sr', 'id', 'district', 'town', 'uc', 'tag', 'larva', 'dengueLarva', 'lat', 'long', 'pics', 'timeDiff', 'user', 'activityTime', 'bogus']
 
+        // Iterating through columns
         $(rowSelector).each((parentIdx, parentElm) => {
             let keyIdx = 0
             const activity = {}
@@ -115,10 +103,19 @@ puppeteer.launch({
                     const beforePic = $('a:first-child', $(childElm).html()).attr('href')
                     const afterPic = $('a:nth-child(2)', $(childElm).html()).attr('href')
                     picsBoth.push({
-                        'before': `https://dashboard.tracking.punjab.gov.pk/${beforePic}`,
-                        'after': `https://dashboard.tracking.punjab.gov.pk/${afterPic}`
+                        'before': `https://dashboard.tracking.punjab.gov.pk${beforePic}`,
+                        'after': `https://dashboard.tracking.punjab.gov.pk${afterPic}`
                     })
                     tdValue = picsBoth
+                }
+
+                if (keyIdx === 14) {
+                    const bogus = $('button', $(childElm).html()).attr('onclick')
+                    if (bogus === undefined) {
+                        tdValue = "Bogus Activity"
+                    } else {
+                        tdValue = `https://dashboard.tracking.punjab.gov.pk${bogus.substr(17)}`
+                    }
                 }
 
                 if (tdValue) {
